8396390ff96469c80727adb11f7cf390c306d200
lekofox <leandro_dias9@hotmail.com>
2021-07-25 22:15:32 -0300

    refactor: message fix from password test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -31,6 +31,22 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
+
+  test('Should return 400 if no password is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Denis',
+        email: 'leandro@leandro.com.br',
+        // password: 'l@lcombr',
+        passwordConfirmation: 'l@lcombr'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
+  })
+
   test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {

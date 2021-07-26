618c94ec1871566c6d8b74f1088d015a220bd396
lekofox <leandro_dias9@hotmail.com>
2021-07-25 21:13:35 -0300

    feat: ensure SignUpController returns 400 if no name provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        // name: 'Leandro',
+        email: 'leandro@leandro.com.br',
+        password: 'l@lcombr',
+        passwordConfirmation: 'l@lcombr'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
